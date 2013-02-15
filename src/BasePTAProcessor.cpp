bce2e60e773aca8ac1119d987803b378e498df3d
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2013-02-15 16:27:56 +0200

    cosider interval order when calculating adjacency

--- src/BasePTAProcessor.cpp
+++ src/BasePTAProcessor.cpp
@@ -51,7 +51,8 @@ double BasePTAProcessor::length(int interval) const {
 }
 
 bool BasePTAProcessor::adjacent(int i, int j) const {
-    return start[j] - end[i] <= adjacency_treshold;
+    double distance = start[j] - end[i];
+    return (distance >= 0) && (distance <= adjacency_treshold);
 }
 
 double BasePTAProcessor::merged_score(int i, int j) const {
