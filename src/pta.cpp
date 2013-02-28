42ba91ea085d162e6d32413a22ed08c1a13cff5b
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2013-02-28 23:49:34 +0200

    add BEGIN/END_RCPP guards

--- src/pta.cpp
+++ src/pta.cpp
@@ -5,13 +5,17 @@ using namespace Rcpp;
 using namespace std;
 
 SEXP PTA(SEXP start, SEXP end, SEXP score, SEXP count, SEXP error, SEXP adjacency_treshold) {
+BEGIN_RCPP
     OptimalPTAProcessor p(start, end, score, count, error, adjacency_treshold);
     p.run();
     return p.get_result();
+END_RCPP
 }
 
 SEXP gPTA(SEXP start, SEXP end, SEXP score, SEXP count, SEXP error, SEXP adjacency_treshold) {
+BEGIN_RCPP
     GreedyPTAProcessor p(start, end, score, count, error, adjacency_treshold);
     p.run();
     return p.get_result();
+END_RCPP
 }
