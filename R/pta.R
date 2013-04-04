31e7bcfc4fd64bf3cab8fe7e999e74ff7f8f865f
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2013-04-04 11:32:12 +0300

    use more universal score function

--- R/pta.R
+++ R/pta.R
@@ -1,12 +1,12 @@
 library(IRanges)
 
 PTA <- function(data, count=1, error=Inf, adjacency.treshold=1){
-    result <- .Call("PTA", start(data), end(data), data[["score"]], count, error, adjacency.treshold, PACKAGE = "pta")
+    result <- .Call("PTA", start(data), end(data), score(data), count, error, adjacency.treshold, PACKAGE = "pta")
     RangedData(IRanges(start=result$start, end=result$end), score=result$score)
 }
 
 gPTA <- function(data, count=1, error=Inf, adjacency.treshold=1){
-    result <- .Call("gPTA", start(data), end(data), data[["score"]], count, error, adjacency.treshold, PACKAGE = "pta")
+    result <- .Call("gPTA", start(data), end(data), score(data), count, error, adjacency.treshold, PACKAGE = "pta")
     RangedData(IRanges(start=result$start, end=result$end), score=result$score)
 }
 
